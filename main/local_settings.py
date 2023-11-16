6cf84f953f21c38c0337ad680ed26e68d5e36522
ndRaa15 <fuwafu212@gmail.com>
2023-11-16 22:48:52 +0700

    update cors

--- main/local_settings.py
+++ main/local_settings.py
@@ -46,7 +46,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "main.middleware.corsMiddleware",
 ]
 
 ROOT_URLCONF = "main.urls"

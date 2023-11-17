62932f8f5f4dbc18befe2c933a95ed1152dc65a2
ndRaa15 <fuwafu212@gmail.com>
2023-11-17 11:31:03 +0700

    add depedencies

--- main/views.py
+++ main/views.py
@@ -10,6 +10,7 @@ import pandas as pd
 @api_view(['POST'])
 def analysis_diabetes(request):
     if request.method == 'POST':
+        
         serializer = DiabetesSerializer(data=request.data)
         if serializer.is_valid():
             df = pd.DataFrame({
@@ -23,13 +24,16 @@ def analysis_diabetes(request):
                 'Age': [serializer.validated_data['Age']]
             })
             model = joblib.load('diabetics-prediction-rfr.joblib')
+            
             try:
                 result = model.predict(df)
                 serializer.validated_data['Percentage'] = result[0] * 100
                 serializer.save()
                 return JsonResponse({"Data": serializer.data}, status=status.HTTP_201_CREATED)
+            
             except Exception as e:
                 return JsonResponse({"error": f"Prediction error: {str(e)}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 def CalculateBMI(weight, height):

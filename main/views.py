2ffa2714cfe6254d2e1a0ea9dd2bc3113a61c93d
ndRaa15 <fuwafu212@gmail.com>
2023-11-17 11:17:04 +0700

    adding model

--- main/views.py
+++ main/views.py
@@ -4,18 +4,33 @@ from .serializers import DiabetesSerializer
 from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
+import joblib
+import pandas as pd
 
 @api_view(['POST'])
 def analysis_diabetes(request):
     if request.method == 'POST':
         serializer = DiabetesSerializer(data=request.data)
-        # get all component
-        # input into model
-        # return the percentage
-
-
         if serializer.is_valid():
-            serializer.validated_data['Percentage'] = 1.0
-            serializer.save()
-            return JsonResponse({"Data" : serializer.data},status=status.HTTP_201_CREATED)
-        return JsonResponse(serializer.errors, status=400)
+            df = pd.DataFrame({
+                'Pregnancies': [serializer.validated_data['Pregnancies']],
+                'Glucose': [serializer.validated_data['Glucose']],
+                'BloodPressure': [serializer.validated_data['BloodPressure']],
+                'SkinThickness': [serializer.validated_data['SkinThickness']],
+                'Insulin': [serializer.validated_data['Insulin']],
+                'BMI': [CalculateBMI(serializer.validated_data['Weight'], serializer.validated_data['Height'])],
+                'DiabetesPedigreeFunction': [serializer.validated_data['DiabetesPedigreeFunction']],
+                'Age': [serializer.validated_data['Age']]
+            })
+            model = joblib.load('diabetics-prediction-rfr.joblib')
+            try:
+                result = model.predict(df)
+                serializer.validated_data['Percentage'] = result[0] * 100
+                serializer.save()
+                return JsonResponse({"Data": serializer.data}, status=status.HTTP_201_CREATED)
+            except Exception as e:
+                return JsonResponse({"error": f"Prediction error: {str(e)}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+def CalculateBMI(weight, height):
+    return weight/(height*height)
